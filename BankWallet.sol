55016e65bc17dca03909ffc2bd63a668ecf018a4
Sridhar Babu Kolapalli <ksridharbabuus@gmail.com>
2018-01-21 10:17:02 +0530

    Added Ownable Code to the contract

--- BankWallet.sol
+++ BankWallet.sol
@@ -2,19 +2,23 @@ pragma solidity ^0.4.18;
 
 import "./IBankWallet.sol";
 import "./SafeMath.sol";
+import "./Ownable.sol";
 
-contract BankWallet is IBankWallet {
+contract BankWallet is IBankWallet, Ownable {
 
      using SafeMath for uint256;
      
      function BankWallet() public {
          owner = msg.sender; // Owner of the contract
+         // msg.value
+         totalAmount = 0;
      }
 
      function Deposit(uint256 _amount) public {
          require(_amount >= 0);
          //balances[msg.sender] = balances[msg.sender] + _amount;
          balances[msg.sender] = balances[msg.sender].add(_amount);
+         totalAmount = totalAmount.add(_amount);
      }
      
      function Withdraw(uint256 _amount) public returns (uint256){
@@ -27,12 +31,18 @@ contract BankWallet is IBankWallet {
          
          //balances[msg.sender] = balances[msg.sender] - _amount;
          balances[msg.sender] = balances[msg.sender].sub(_amount);
+         totalAmount = totalAmount.sub(_amount);
          
          return balances[msg.sender];
      }
     
     function GetBalance() public view returns (uint256) {
         return balances[msg.sender];
+    }
+    
+    // Should not be executed by everyone - can be executed by only owner
+    function GetBankBalance() public view onlyOwner returns (uint256){
+        return totalAmount;
     }
     
     function Transfer(address _to, uint256 _amount) public returns (uint256) {
