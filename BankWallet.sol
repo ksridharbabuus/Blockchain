406610566c69b0681fb0f705e9a83d8a82c07ecb
Sridhar Babu Kolapalli <ksridharbabuus@gmail.com>
2018-01-21 10:06:25 +0530

    Updated with Libraries demostration

--- BankWallet.sol
+++ BankWallet.sol
@@ -1,16 +1,20 @@
 pragma solidity ^0.4.18;
 
 import "./IBankWallet.sol";
+import "./SafeMath.sol";
 
 contract BankWallet is IBankWallet {
 
+     using SafeMath for uint256;
+     
      function BankWallet() public {
          owner = msg.sender; // Owner of the contract
      }
 
      function Deposit(uint256 _amount) public {
          require(_amount >= 0);
-         balances[msg.sender] = balances[msg.sender] + _amount;
+         //balances[msg.sender] = balances[msg.sender] + _amount;
+         balances[msg.sender] = balances[msg.sender].add(_amount);
      }
      
      function Withdraw(uint256 _amount) public returns (uint256){
@@ -21,7 +25,8 @@ contract BankWallet is IBankWallet {
          //   balances[msg.sender] = balances[msg.sender] - _amount;
          //}
          
-         balances[msg.sender] = balances[msg.sender] - _amount;
+         //balances[msg.sender] = balances[msg.sender] - _amount;
+         balances[msg.sender] = balances[msg.sender].sub(_amount);
          
          return balances[msg.sender];
      }
@@ -35,8 +40,8 @@ contract BankWallet is IBankWallet {
         require(_amount > 0);
         require(balances[msg.sender] >= _amount);
     
-        balances[msg.sender] = balances[msg.sender] - _amount;
-        balances[_to] = balances[_to] + _amount;
+        balances[msg.sender] = balances[msg.sender].sub(_amount);
+        balances[_to] = balances[_to].add(_amount);
         
         return balances[msg.sender];
     }
